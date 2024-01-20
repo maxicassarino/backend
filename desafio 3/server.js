5d0ab80e8c41f60f62f9c98ba29c1592fec75e38
maxicassarino <maximocassarino@gmail.com>
2024-01-20 16:39:41 -0300

    Desafio 3

--- desafio 3/server.js
+++ desafio 3/server.js
@@ -1,34 +1,51 @@
 const express = require("express")
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const app = express()
 
 const filePath = '../desafio2/productos.json';
 
-const data = fs.readFileSync(filePath, 'utf-8');
-const productos = JSON.parse(data);
-
-app.get('/products', (req, res) => {
-    let limite = parseInt(req.query.limit);
-    if (!isNaN(limite) && limite > 0) {
-        productosLimitados = productos.slice(0, limite);
-        return res.send(productosLimitados);
-    } else {
-        return res.send(productos);
+const lectura = async () => {
+    try {
+        const data = await fs.readFile(filePath, 'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error("Error de lectura: ", error);
+        return [];
     }
-})
+};
 
 
-app.get('/products/:id', (req, res) => {
-    const productoId = req.params.id;
-    let producto = productos.find((p) => p.id == productoId);
-    if (producto) {
-        return res.send(producto);
-    } else {
-        return res.send("Producto no encontrado.");
+app.get('/products', async (req, res) => {
+    try {
+        const productos = await lectura();
+        let limite = parseInt(req.query.limit);
+        if (!isNaN(limite) && limite > 0) {
+            const productosLimitados = productos.slice(0, limite);
+            return res.send(productosLimitados);
+        } else {
+            return res.send(productos);
+        }
+    } catch (error) {
+        console.error("Error al obtener productos: ", error);
     }
-})
+});
 
+
+app.get('/products/:id', async (req, res) => {
+    try {
+        const productos = await lectura();
+        const productoId = req.params.id;
+        let producto = productos.find((p) => p.id == productoId);
+        if (producto) {
+            return res.send(producto);
+        } else {
+            return res.send("Producto no encontrado.");
+        }
+    } catch (error) {
+        console.error("Error al obtener producto por ID: ", error);
+    }
+});
 
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Server funcionando en puerto ${PORT}`));
