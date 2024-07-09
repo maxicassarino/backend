e7f4943df7c0873e1bb8e14870fff0457994f901
maxicassarino <maximocassarino@gmail.com>
2024-07-09 15:31:45 -0300

    Entrega Final

--- src/server.js
+++ src/server.js
@@ -139,7 +139,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
 
 // Puerto
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server funcionando en puerto ${PORT}`));
 
 
